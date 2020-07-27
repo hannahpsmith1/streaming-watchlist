30552480bdbfebfb4c7294b573e48accc04aeb7b
Hannah Smith <hannahpsmith1@gmail.com>
2020-07-26 18:56:26 -0700

    deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,16 @@
 var mysql = require ("mysql"); 
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "streaming_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "streaming_db"
+// });
 
 // for Heroku
-// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
 
 // Make connection.
 connection.connect(function (err) {
