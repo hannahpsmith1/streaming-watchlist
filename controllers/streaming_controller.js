4774a925cb1ad216eca4776df167f563387e42ac
Hannah Smith <hannahpsmith1@gmail.com>
2020-07-26 19:20:08 -0700

    looking for table

--- controllers/streaming_controller.js
+++ controllers/streaming_controller.js
@@ -5,28 +5,28 @@ var router = express.Router();
 var stream = require ("../models/streaming.js");
 
 router.get("/", function(req, res){
-  stream.all(function(data){
-    // stream.all (function(media){
-      var hbsObject = {
-        media: data
-      };
-      console.log(hbsObject);
-        // console.log(media)
-        // res.render("index", media) 
-        res.render("index", hbsObject);
+  // stream.all(function(data){
+    stream.all (function(media){
+      // var hbsObject = {
+      //   media: data
+      // };
+      // console.log(hbsObject);
+        console.log(media)
+        res.render("index", media) 
+        // res.render("index", hbsObject);
     });
   });
 
-router.post("/api/media", function(req, res) {
-    media.create([
-      "name", "watched"
-    ], [
-      req.body.name, req.body.watched
-    ], function(result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-    });
-  });
+// router.post("/api/media", function(req, res) {
+//     media.create([
+//       "name", "watched"
+//     ], [
+//       req.body.name, req.body.watched
+//     ], function(result) {
+//       // Send back the ID of the new quote
+//       res.json({ id: result.insertId });
+//     });
+//   });
 
 
 module.exports = router;
